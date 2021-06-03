8a23677ef1f058a0cdf556ffced0c8297ad84a58
东灯 <43312495+Lampese@users.noreply.github.com>
2021-06-04 01:04:38 +0800

    Update attcker.js

--- attcker.js
+++ attcker.js
@@ -1,11 +1,8 @@
 function attcker_go(creep,where){
-     if(creep.room.name!=where){
-        creep.moveTo(new RoomPosition(25,20,where));
-        return;
-    }
-    else{
+     if(creep.room.name!=where){creep.moveTo(new RoomPosition(25,20,where));
+     else{
         const target=creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         if(creep.rangedAttack(target)==ERR_NOT_IN_RANGE&&target)creep.moveTo(target,{reusePath:0});
-    }
+     }
 }
 exports.Go=attcker_go;
